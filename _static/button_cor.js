6752e7b0b48516d4133accef9efc2379b6434f61
fortierq <fortierq@users.noreply.github.com>
2023-02-18 21:34:18 +0000

    deploy: f4682a6fe9d9a8e8c57b069597cda437a4b47a4a

--- _static/button_cor.js
+++ _static/button_cor.js
@@ -1,15 +1,14 @@
 function url(pdf) {
-    const url_repo_pdf = 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://raw.githubusercontent.com/mp-info/mp-info.github.io/main/files/'
-    return url_repo_pdf + pdf + '#zoom=page-fit&pagemode=none';
+    return 'https://mozilla.github.io/pdf.js/web/viewer.html?file=' + pdf + '#zoom=page-fit&pagemode=none';
 }
 
 function button_cor(pdf, id_pdf, id_button) {
     let state = "Correction";
     const pub = {};
-    $(`#${id_pdf}`).attr('src', url(`${pdf}.pdf`));
+    $(`#${id_pdf}`).attr('src', url(pdf));
     $(`#${id_button}`).prop('value', state);
     pub.switch = function () {
-        $(`#${id_pdf}`).attr('src', url(`${pdf}${state == "Correction" ? "_cor" : ""}.pdf`));
+        $(`#${id_pdf}`).attr('src', url(`${pdf.substring(0, pdf.length-4)}${state == "Correction" ? "_cor" : ""}.pdf`));
         state = state == "Énoncé" ? "Correction" : "Énoncé";
         $(`#${id_button}`).prop('value', state);
     };
